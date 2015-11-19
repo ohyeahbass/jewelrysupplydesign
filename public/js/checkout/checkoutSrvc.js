71d6bab1f47a84cb98b614b681726d030b703810
Travis E <ohyeahbass@yahoo.com>
2015-11-18 23:04:17 -0700

    kk

--- public/js/checkout/checkoutSrvc.js
+++ public/js/checkout/checkoutSrvc.js
@@ -4,7 +4,7 @@ adminApp.service('checkoutSrvc', function($http){
 	this.checkOut=function(prodInfo){
 		return $http({
 			method:"POST",
-			url:'http://localhost:3000/order',
+			url:'/order',
 			data: prodInfo
 		})
 	}
