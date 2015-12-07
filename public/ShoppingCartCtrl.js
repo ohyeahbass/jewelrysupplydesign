f7faa15dd61f61177ec90cc55cdbfe664445e61a
Travis E <ohyeahbass@yahoo.com>
2015-12-07 11:20:38 -0700

    reworked

--- public/ShoppingCartCtrl.js
+++ public/ShoppingCartCtrl.js
@@ -2,24 +2,20 @@ var adminApp=angular.module('adminApp');
 adminApp.controller('ShoppingCartCtrl', function($scope, cartService){
 	
 
-	console.log("hi")
-	$scope.cartCount=0
+	console.log("hi");
+	$scope.cartCount=0;
 	function totalCart(){
-		var toats=JSON.parse(localStorage.getItem('text'))
-		var array=[]
+		var toats=JSON.parse(localStorage.getItem('text'));
+		var array=[];
 						
 		for(var prop in toats){		
-			array.push(toats[prop].qty)	
+			array.push(toats[prop].qty);	
 		}
 		for(var i=0;i<array.length;i++){
-			var num=Number(array[i])
-			$scope.cartCount+=num
-		}
-		return $scope.cartCount
+			var num=Number(array[i]);
+			$scope.cartCount+=num;
+		};
+		return $scope.cartCount;
 	}
-	totalCart()
-	
-	
-			
-			
+	totalCart();		
 })
